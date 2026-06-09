9100117abd49914ab6eff8fa7e8fc6eabea9a486
sudo-unravel <petobalint09@protonmail.com>
2026-06-09 19:42:52 +0200

    update themes

--- lua/theme/gruvbox-material.lua
+++ lua/theme/gruvbox-material.lua
@@ -1,17 +1,21 @@
 return {
-	"sainnhe/gruvbox-material",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.g.gruvbox_material_better_performance = 1
-		vim.g.gruvbox_material_foreground = "material"
-		vim.g.gruvbox_material_background = "medium"
-		vim.g.gruvbox_material_float_style = "blend"
-		vim.g.gruvbox_material_statusline_style = "default" -- "material"
-		vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-		vim.g.gruvbox_material_disable_terminal_colors = 1
-		vim.g.gruvbox_material_transparent_background = 1
-
-		vim.cmd.colorscheme("gruvbox-material")
-	end,
+	{
+		"f4z3r/gruvbox-material.nvim",
+		name = "gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			italics = true,
+			contrast = "medium",
+			background = {
+				transparent = true,
+			},
+			float = {
+				force_background = true,
+			},
+			signs = {
+				force_background = false,
+			},
+		},
+	},
 }
