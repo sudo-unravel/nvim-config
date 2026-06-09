4407dece9adab29957269e7627429cb3e265d8ae
sudo-unravel <petobalint09@protonmail.com>
2026-06-09 19:43:03 +0200

    minor changes

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -4,9 +4,16 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"yavorski/lualine-macro-recording.nvim",
 	},
-	opts = {
-		sections = {
-			lualine_c = { "macro_recording", "%S" },
-		},
-	},
+	opts = function()
+		local theme = require("gruvbox-material.lualine").theme("medium")
+
+		return {
+			options = {
+				theme = theme,
+			},
+			sections = {
+				lualine_c = { "macro_recording", "%S" },
+			},
+		}
+	end,
 }
